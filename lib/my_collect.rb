051597ec9eec758034ee9bcc6d7a3f1e39362841
Kristina Frederick <kristinafrederick@icloud.com>
2020-06-24 21:48:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,12 @@
 def my_collect(languages)
     i = 0
     collection = ['ruby', 'javascript', 'python', 'objective-c']
+    my_collect(collection) do |lang|
+  lang.upcase
+  end
     while i < languages.length
       i += 1
       yield(languages[i])
-      my_collect(collection) do |lang|
-    lang.upcase
-    end
+    
 end
 end
